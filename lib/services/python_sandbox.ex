332b0f7433c874306b6054709773dc303d5bb163
arcstep <43801@qq.com>
2024-07-13 18:01:54 +0800

    feat: Update Neverland.PythonSandbox to handle script output and events

--- lib/services/python_sandbox.ex
+++ lib/services/python_sandbox.ex
@@ -3,51 +3,86 @@ defmodule Neverland.PythonSandbox do
 
   def start_link(opts \\ []) do
     name = opts |> Keyword.get(:name, __MODULE__)
-    scripts_dir = opts |> Keyword.get(:scripts_dir, "/Users/xuehongwei/github/neverland/priv/scripts")
+    scripts_dir = opts |> Keyword.get(:scripts_dir, "./priv/scripts")
     GenServer.start_link(__MODULE__, %{scripts_dir: scripts_dir, buffer: "", port: nil}, Keyword.put_new(opts, :name, name))
   end
 
-  @spec init(map()) :: {:ok, map()}
   def init(state) do
-    {:ok, state}
+    # 确保在初始状态中包含batches键，初始值为空映射
+    new_state = Map.put_new(state, :batches, %{})
+    {:ok, new_state}
   end
 
   def run_script(pid, script_name) do
     GenServer.call(pid, {:run_script, script_name})
   end
 
-  def handle_call({:run_script, script_name}, _from, %{scripts_dir: scripts_dir} = state) do
+  def handle_call({:run_script, script_name}, from, %{scripts_dir: scripts_dir, batches: batches} = state) do
     python_script_path = Path.join([scripts_dir, script_name])
-    port = Port.open({:spawn, "python3 #{python_script_path}"}, [:binary, :exit_status])
-    {:reply, :ok, %{state | port: port}}
+    port = Port.open({:spawn, "python3 -u #{python_script_path}"}, [:binary, :exit_status])
+
+    # 使用Port引用作为键，存储caller信息
+    new_batches = Map.put(batches, port, %{caller: from, output: "", logs: []})
+
+    # 更新state，包括新的batches映射
+    new_state = Map.put(state, :batches, new_batches)
+
+    {:reply, :ok, new_state}
   end
 
-  def handle_info({_port, {:data, data}}, %{buffer: buffer} = state) do
-    new_buffer = buffer <> data
-    {events, new_buffer} = parse_events(new_buffer)
-    Enum.each(events, &handle_event/1)
-    {:noreply, %{state | buffer: new_buffer}}
+  def handle_info({port, {:data, data}}, %{batches: batches} = state) do
+    # IO.puts(">>>>>>>>>>>")
+    # IO.puts(inspect(state))
+    # IO.puts(inspect(port))
+    # IO.puts(data)
+    # IO.puts(">>>>>>>>>>>")
+    case Map.get(batches, port) do
+      nil ->
+        {:noreply, state}
+      %{caller: _caller, output: current_output, logs: current_logs} = batch ->
+        case Regex.run(~r/>-\[(.*?)\]>>(.*)/, data, capture: :all_but_first) do
+          nil ->
+            # 如果没有匹配到数据，可以选择忽略或者以不同的方式处理
+            # IO.puts(">>>>")
+            # IO.puts(data)
+            # IO.puts("<<<<")
+            {:noreply, state}
+          [event, data] ->
+            # 将事件和数据作为元组添加到logs中
+            new_logs = [{event, data} | current_logs]
+
+            # 如果事件类型是text或final，则将数据追加到output中
+            # IO.puts(">-[#{event}]>>")
+            new_output =
+              case event do
+                "text" ->
+                  IO.puts(data)
+                  current_output <> data
+                "final" ->
+                  IO.puts("\n" <> data)
+                  current_output <> data
+                _ ->
+                  IO.write(data)
+                  current_output
+              end
+
+            # 更新该批次的信息
+            new_batch = Map.put(batch, :output, new_output)
+            new_batch = Map.put(new_batch, :logs, new_logs)
+            new_batches = Map.put(batches, port, new_batch)
+
+            # 更新state
+            new_state = Map.put(state, :batches, new_batches)
+
+            {:noreply, new_state}
+        end
+    end
   end
 
   def handle_info({_port, {:exit_status, status}}, state) do
-    IO.puts("Python script exited with status: #{status}")
+    IO.puts("\nPython script exited with status: #{status}")
+    IO.puts(inspect(state))
     {:stop, :normal, state}
   end
 
-  @spec handle_event(binary()) :: :ok
-  def handle_event(event) do
-    [event_name, data] = String.split(event, "]>> ", parts: 2)
-    IO.puts("Event: #{event_name}, Data: #{data}")
-  end
-
-  def parse_events(buffer) do
-    split_buffer = String.split(buffer, "\n", trim: true)
-    Enum.reduce(split_buffer, {[], ""}, fn line, {events, buffer} ->
-      if String.starts_with?(line, ">-[") do
-        if buffer != "", do: {events ++ [buffer], line}, else: {events, line}
-      else
-        {events, buffer <> "\n" <> line}
-      end
-    end)
-  end
 end
--- priv/scripts/demo.py
+++ priv/scripts/demo.py
@@ -1,9 +1,16 @@
+import time  # 导入time模块
+
 # script.py
 def emit_event(event_name, data):
-    print(f">-[{event_name}]>> {data}")
+    print(f">-[{event_name}]>>{data}")
+    time.sleep(0.5)  # 在打印之后休眠0.1秒
 
-emit_event('start', 'Script started')
-# ... 在脚本中执行操作 ...
-emit_event('progress', '50% complete')
-# ... 更多操作 ...
-emit_event('finish', 'Script finished')
+emit_event('text', 'Human: ')
+emit_event('chunk', '你')
+emit_event('chunk', '说的')
+emit_event('chunk', '都对')
+emit_event('chunk', '!')
+emit_event('chunk', '\n')
+emit_event('chunk', '很对！')
+emit_event('final', '你说的都对!\n很对！')
+emit_event('info', '这是一个脚本测试。\n')
