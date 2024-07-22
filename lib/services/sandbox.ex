dbd90d48d715b77d600c0b879d4f700dcabea23f
arcstep <43801@qq.com>
2024-07-22 19:41:57 +0800

    feat: Remove unused code and refactor Neverland.Sandbox and Neverland.SandboxCycle

--- lib/services/sandbox.ex
+++ lib/services/sandbox.ex
@@ -1,21 +1,4 @@
 defmodule Neverland.Sandbox do
-  def invoke_command(%{port: port, queue: queue} = state) do
-    case queue do
-      [] ->
-        state
-      [head | queue_rest] ->
-        case head do
-          {:input, in_text, :reply_to, reply_to} ->
-            data = {self(), {:command, "#{in_text}\n"}}
-            IO.puts(inspect(data))
-            send(port, data)
-            state
-              |> Map.put(:queue, queue_rest)
-              |> Map.put(:cur_reply_to, reply_to)
-        end
-    end
-  end
-
   def process_data(data) do
     case Regex.run(~r/>-\[(.*?)\]>>(.*)/, data, capture: :all_but_first) do
       nil -> {"text", data}
@@ -23,13 +6,11 @@ defmodule Neverland.Sandbox do
     end
   end
 
-  def process_event(event, processed_data, state, reply_to \\ nil, current_output \\ "") do
+  def process_event(event, processed_data, thread_id, reply_to \\ nil, current_output \\ "") do
     if reply_to do
-      send(reply_to, {event, processed_data})
+      send(reply_to, {:thread_id, thread_id, :event, event, :output, processed_data})
     end
     case event do
-      "end" ->
-        invoke_command(state)
       "text" ->
         IO.puts(processed_data)
         current_output <> processed_data
