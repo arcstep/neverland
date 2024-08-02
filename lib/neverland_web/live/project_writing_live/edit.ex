fafa480d054d41e5805cd089153365c14236ec3d
arcstep <43801@qq.com>
2024-08-02 21:47:59 +0800

    refactor: Update project file path retrieval method

--- lib/neverland_web/live/project_writing_live/edit.ex
+++ lib/neverland_web/live/project_writing_live/edit.ex
@@ -5,7 +5,20 @@ defmodule NeverlandWeb.Project.WritingLive.Edit do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, thread_id} =
+      Neverland.SandboxPython.run(:sandbox_python, "chat_with_textlong.py", self())
+
+    IO.inspect("mounting...thread_id: #{thread_id}")
+
+    markdown_content = """
+    # 这是一个标题
+
+    这是一个段落，用于展示如何在 Phoenix LiveView 中渲染 Markdown 内容。
+    """
+
+    html_content = Earmark.as_html!(markdown_content)
+
+    {:ok, assign(socket, :html_content, html_content)}
   end
 
   @impl true
@@ -18,4 +31,10 @@ defmodule NeverlandWeb.Project.WritingLive.Edit do
 
   defp page_title(:show), do: "🦋 项目文档查看"
   defp page_title(:edit), do: "🦋 AI写作"
+
+  @impl true
+  def handle_info(message, socket) do
+    IO.inspect("handling info...#{inspect(message)}")
+    {:noreply, socket}
+  end
 end
