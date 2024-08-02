e705de8da3a461f876f29ab7fb0c6112c6029c09
arcstep <43801@qq.com>
2024-08-02 22:01:49 +0800

    refactor: Update project_writing_live/edit.ex to handle Python sandbox thread output

--- lib/neverland_web/live/project_writing_live/edit.ex
+++ lib/neverland_web/live/project_writing_live/edit.ex
@@ -8,17 +8,10 @@ defmodule NeverlandWeb.Project.WritingLive.Edit do
     {:ok, thread_id} =
       Neverland.SandboxPython.run(:sandbox_python, "chat_with_textlong.py", self())
 
-    IO.inspect("mounting...thread_id: #{thread_id}")
-
-    markdown_content = """
-    # 这是一个标题
-
-    这是一个段落，用于展示如何在 Phoenix LiveView 中渲染 Markdown 内容。
-    """
-
-    html_content = Earmark.as_html!(markdown_content)
-
-    {:ok, assign(socket, :html_content, html_content)}
+    {:ok,
+     socket
+     |> assign(:html_content, "")
+     |> assign(:thread_id, thread_id)}
   end
 
   @impl true
@@ -33,8 +26,17 @@ defmodule NeverlandWeb.Project.WritingLive.Edit do
   defp page_title(:edit), do: "🦋 AI写作"
 
   @impl true
-  def handle_info(message, socket) do
-    IO.inspect("handling info...#{inspect(message)}")
-    {:noreply, socket}
+  def handle_info({:thread_id, _thread_id, :event, _event, :output, output}, socket) do
+    IO.inspect("handling info...#{inspect(output)}")
+
+    _markdown_content = """
+    # 这是一个标题
+
+    这是一个段落，用于展示如何在 Phoenix LiveView 中渲染 Markdown 内容。
+    """
+
+    html_content = Earmark.as_html!(output)
+
+    {:noreply, assign(socket, :html_content, html_content)}
   end
 end
