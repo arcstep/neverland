0a860a47536685f0ed14781fda0aeab88825d0d8
arcstep <43801@qq.com>
2024-08-09 15:51:54 +0800

    refactor: Update Neverland configuration for textlong_root path

--- lib/neverland/project_resource.ex
+++ lib/neverland/project_resource.ex
@@ -1,23 +1,29 @@
-defmodule Neverland.Sandbox.Resource do
-  # 假设这是项目文件夹的根路径
+defmodule Neverland.Project.Resource do
+  # 项目文件夹的配置
   @project_base_path Application.compile_env!(:neverland, __MODULE__)[:textlong_root]
 
-  # 获取项目文件夹下的文件列表
+  # 获取项目文件列表
   def file_list(project_id \\ "") do
     project_path = project_path(project_id)
     {:ok, files} = File.ls(project_path)
 
-    Enum.map(files, fn file ->
+    files
+    |> Enum.filter(&filter_files/1)
+    |> Enum.map(fn file ->
       full_path = Path.join(project_path, file)
 
       %{
         type: if(File.dir?(full_path), do: "dir", else: "file"),
         name: file,
         path: full_path,
-        # 假设使用UUID作为文件ID
-        file_id: UUID.uuid5(:dns, full_path)
+        id: UUID.uuid5(:dns, full_path)
       }
     end)
+  end
+
+  defp filter_files(file_name) do
+    excluded_files = ["project_config.yml", "project_list.yml", "__LOG__"]
+    not Enum.member?(excluded_files, file_name)
   end
 
   # 创建文件
